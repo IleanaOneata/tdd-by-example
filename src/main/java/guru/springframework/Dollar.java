0ebe6155b507abef9f9e8104c218c6cede232d57
Ileana Oneata <ileana.oneata@yahoo.com>
2021-10-26 14:39:05 +0300

    Multi-Currency Money and Degenerate objects

---
+++ src/main/java/guru/springframework/Dollar.java
@@ -0,0 +1,18 @@
+package guru.springframework;
+
+/**
+ * @author ileanaoneata
+ */
+
+public class Dollar {
+
+    int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    Dollar times(int multiplier) {
+        return new Dollar(amount * multiplier);
+    }
+}
